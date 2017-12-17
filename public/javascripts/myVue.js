31bffbf61d63218d123ab842f29d43f8baff45c1
MarkusBuechler <markus.buechler@mail.de>
2017-12-17 16:52:40 +0100

    added vue support

---
+++ public/javascripts/myVue.js
@@ -0,0 +1,35 @@
+/**
+ * document ready needed !!!!!! aaaaarrrrr
+ */
+
+$(document).ready(function () {
+
+    new Vue({
+        el: '#bar-1',
+        data: {
+            message: '11, 3'
+        }
+    })
+
+    new Vue({
+        el: '#bar-2',
+        data: {
+            message: 'Aufrüsten'
+        }
+    })
+
+    new Vue({
+        el: '#bar-3',
+        data: {
+            message: '3'
+        }
+    })
+
+    new Vue({
+        el: '#message-1',
+        data: {
+            message: 'The best game ! '
+        }
+    })
+
+})
