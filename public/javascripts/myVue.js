1963a93fe94a7298cf817b2ad98e1b059173bd72
MarkusBuechler <markus.buechler@mail.de>
2017-12-17 17:20:40 +0100

    fixed codacy 2

--- public/javascripts/myVue.js
+++ public/javascripts/myVue.js
@@ -2,34 +2,39 @@
  * document ready needed !!!!!! aaaaarrrrr
  */
 
+let barElement1;
+let barElement2;
+let barElement3;
+let message1;
+
 $(document).ready(function () {
 
-    let barElement1 = new Vue({
+    barElement1 = new Vue({
         el: "#bar-1",
         data: {
             message: "11, 3"
         }
-    })
+    });
 
-    let barElement2 = new Vue({
+    barElement2 = new Vue({
         el: "#bar-2",
         data: {
             message: "Aufrüsten"
         }
-    })
+    });
 
-    let barElement3 = new Vue({
+    barElement3 = new Vue({
         el: "#bar-3",
         data: {
             message: "3"
         }
-    })
+    });
 
-    let message1 = new Vue({
+    message1 = new Vue({
         el: "#message-1",
         data: {
             message: "The best game ! "
         }
-    })
+    });
 
 })
