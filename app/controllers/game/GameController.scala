b69d76b1bcc81ce9b150a0af0c7fd671e3f04265
Artur O <ar431ott@htwg-konstanz.de>
2017-12-30 15:38:45 +0100

    fixed codacy issues

--- app/controllers/game/GameController.scala
+++ app/controllers/game/GameController.scala
@@ -9,26 +9,21 @@ import play.api.i18n.{ I18nSupport, Messages }
 import utils.auth.DefaultEnv
 
 import play.api.libs.streams.ActorFlow
-import akka.actor.ActorSystem
 import akka.stream.Materializer
 import akka.actor._
 import com.mohiva.play.silhouette.api.actions.SecuredRequest
 import com.mohiva.play.silhouette.api.{ LogoutEvent, Silhouette }
 import scala.concurrent.Future
+import scala.collection.mutable.ListBuffer
 
 import de.htwg.se.scala_risk.model.impl.{ World => ImplWorld }
 import de.htwg.se.scala_risk.controller.impl.{ GameLogic => ImplGameLogic }
 import services.game.{ SocketActor => GameSocketActor }
 import services.game.GameManager
-import scala.collection.mutable._
-import models._
 import models.shared.GamesShared
+import models._
 
 /**
- * This controller demonstrates how to use dependency injection to
- * bind a component into a controller class. The class creates an
- * `Action` that shows an incrementing count to users. The [[Counter]]
- * object is injected by the Guice dependency injection system.
  */
 @Singleton
 class GameController @Inject() (cc: ControllerComponents, silhouette: Silhouette[DefaultEnv])(implicit system: ActorSystem, mat: Materializer, webJarsUtil: WebJarsUtil, assets: AssetsFinder) extends AbstractController(cc) with I18nSupport {
@@ -51,55 +46,63 @@ class GameController @Inject() (cc: ControllerComponents, silhouette: Silhouette
       case None => Future.successful(BadRequest(""))
       case Some(post) => {
         if (post.contains("game_selected")) {
-          val gameSelected = post.get("game_selected").getOrElse(List[String]("-1")).mkString;
-          if (!GamesShared.getGames.exists(_.id.equals(gameSelected))) {
-            // No game found
-            // TODO: kommentare entfernen
-            //Redirect(routes.game.GameController.index(), 302)
-            Future.successful(BadRequest(""))
-          } else {
-            GamesShared.getGames.filter(_.id.equals(gameSelected)).headOption match {
-              // No game found
-              case None => Future.successful(BadRequest(""))
-              case Some(gameModel) => {
-
-                post.get("player_name") match {
-                  // No player found
-                  case None => Future.successful(BadRequest(""))
-                  case Some(player) => {
-                    gameModel.player += PlayerModel(player.mkString)
-
-                    println(GamesShared.getGames.toString)
-                    println(player.mkString + " ist dem Spiel "
-                      + gameSelected + " beigetreten")
-                    // TODO: kommentare entfernen
-                    //Redirect(routes.game.GameController.game(), 302).withSession("user" -> player.mkString)
-                    Future.successful(BadRequest(""))
-                  }
-                }
-              }
-            }
-          }
+          selectedGame(post.get("game_selected").getOrElse(List[String]("-1")).mkString)
+        } else if (post.contains("game_name")) {
+          selectedGame(post.get("game_name").getOrElse(List[String]("-1")).mkString)
         } else {
-          post.get("player_name") match {
+          Future.successful(BadRequest(""))
+        }
+      }
+    }
+  }
+
+  private[this] def createGame(gameName: String): Future[Result] = {
+    if (gameName.equals("-1") || gameName.length == 0) Future.successful(BadRequest(""))
+    else {
+      val playerList = ListBuffer[PlayerModel]()
+      playerList += PlayerModel("player.mkString")
+      val world = new ImplWorld()
+      val gameName = "mein Spiel"
+      GamesShared.addGame(GameModel(gameName, playerList,
+        Some(actorSystem.actorOf(Props(new GameManager(new ImplGameLogic(world), playerList)), name = gameName.replaceAll("\\s", "")))
+      ))
+
+      println("player.mkString" + " hat ein Spiel gestartet")
+
+      // TODO: kommentare entfernen
+      //Redirect(routes.game.GameController.game(), 302).withSession("user" -> player.mkString)
+      Future.successful(BadRequest(""))
+    }
+  }
+
+  private[this] def selectedGame(gameSelected: String): Future[Result] = {
+    if (!GamesShared.getGames.exists(_.id.equals(gameSelected))) {
+      // No game found
+      // TODO: kommentare entfernen
+      //Redirect(routes.game.GameController.index(), 302)
+      Future.successful(BadRequest(""))
+    } else {
+      GamesShared.getGames.filter(_.id.equals(gameSelected)).headOption match {
+        // No game found
+        case None => Future.successful(BadRequest(""))
+        case Some(gameModel) => {
+
+          // TODO: add player
+          /*post.get("player_name") match {
             // No player found
             case None => Future.successful(BadRequest(""))
             case Some(player) => {
-              val playerList = ListBuffer[PlayerModel]()
-              playerList += PlayerModel(player.mkString)
-              val world = new ImplWorld()
-              val gameName = "mein Spiel"
-              GamesShared.addGame(GameModel(gameName, playerList,
-                Some(actorSystem.actorOf(Props(new GameManager(new ImplGameLogic(world), playerList)), name = gameName.replaceAll("\\s", "")))
-              ))
+              gameModel.player += PlayerModel(player.mkString)
 
-              println(player.mkString + " hat ein Spiel gestartet")
-
+              println(GamesShared.getGames.toString)
+              println(player.mkString + " ist dem Spiel "
+                + gameSelected + " beigetreten")
               // TODO: kommentare entfernen
               //Redirect(routes.game.GameController.game(), 302).withSession("user" -> player.mkString)
               Future.successful(BadRequest(""))
             }
-          }
+          }*/
+          Future.successful(BadRequest(""))
         }
       }
     }
